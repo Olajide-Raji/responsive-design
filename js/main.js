eaac4210c7e11fc75fa3c23b9df1d47683795971
Olajide-Raji <jyderaj@gmail.com>
2023-08-03 13:48:02 +0100

    ist commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//let tIcon = document.getElementById("t-Icon");
+
+
+function openclose() {
+    let menuRight = document.getElementById("menu-right");
+    menuRight.classList.toggle("open");
+
+}
+
+
+
